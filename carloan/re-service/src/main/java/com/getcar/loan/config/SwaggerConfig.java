83bee7536dbe29fe2cd2eb26934674350c43c0d9
swatikavya <kadamswati518@gmail.com>
2023-06-28 16:22:43 +0530

    Resolve Comflict

---
+++ carloan/re-service/src/main/java/com/getcar/loan/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.getcar.loan.config;
+
+public class SwaggerConfig {
+
+	public void swaggerConfiuration() {
+		System.out.println("swaggerConfiuration");
+		System.out.println("changes done by prajwal");
+		System.out.println("changes done by Mangesh");
+		System.out.println("changes done by Mangesh");
+		System.out.println("changes done by Atul");
+		System.out.println("changes done by Mangesh");
+		System.out.println("changes done by Mangesh");
+		System.out.println("changes done by Mangesh");
+		
+	}
+}

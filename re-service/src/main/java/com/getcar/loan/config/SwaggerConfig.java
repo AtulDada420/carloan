8ffd41061d15b21f0e820cefb9943bce63f3313c
Mr-Mangu07 <mangeshparekar07@gmail.com>
2023-06-28 15:47:48 +0530

    changes

--- re-service/src/main/java/com/getcar/loan/config/SwaggerConfig.java
+++ re-service/src/main/java/com/getcar/loan/config/SwaggerConfig.java
@@ -6,5 +6,9 @@ public class SwaggerConfig {
 		System.out.println("swaggerConfiuration");
 		System.out.println("changes done by prajwal");
 		System.out.println("changes done by Mangesh");
+		System.out.println("changes done by Mangesh");
+		System.out.println("changes done by Atul");
+		System.out.println("changes done by Mangesh");
+		System.out.println("changes done by Mangesh");
 	}
 }

83722b5c38218722adf56218585350d1808aa8e2
shreeseema <simapawar1234@gmail.com>
2023-06-28 15:22:10 +0530

    Changes by Seema

---
+++ re-service/src/main/java/com/getcar/loan/ReServiceApplication.java
@@ -0,0 +1,14 @@
+package com.getcar.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReServiceApplication.class, args);
+	
+	}
+
+}
---
+++ re-service/src/main/java/com/getcar/loan/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.getcar.loan.config;
+
+public class SwaggerConfig {
+
+	public void swaggerConfiuration() {
+		System.out.println("swaggerConfiuration"); 
+		System.out.println("Changes Done by Seema");
+	}
+}

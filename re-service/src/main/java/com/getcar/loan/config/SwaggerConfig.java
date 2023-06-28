86221dfc34d4bbb9398419eca11522b2a8728ea7
dorasunanda <sunagujiri22@gmail.com>
2023-06-28 15:29:39 +0530

    commited by sunanda

--- re-service/src/main/java/com/getcar/loan/config/SwaggerConfig.java
+++ re-service/src/main/java/com/getcar/loan/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ public class SwaggerConfig {
 	public void swaggerConfiuration() {
 		System.out.println("swaggerConfiuration");
 		System.out.println("changes done by sunanda---------");
-		System.out.println("**************************");
-		System.out.println("created sucessfully");
+		
 	}
 }

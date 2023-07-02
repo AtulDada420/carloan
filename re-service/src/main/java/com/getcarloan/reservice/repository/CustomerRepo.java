bd78d8680cd9c3b2f62f6bb513dc01fdc55d3e14
Atul@1999 <atulsingle358@gmail.com>
2023-07-02 11:55:37 +0530

    commited by atul

---
+++ re-service/src/main/java/com/getcarloan/reservice/repository/CustomerRepo.java
@@ -0,0 +1,11 @@
+package com.getcarloan.reservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.getcarloan.reservice.entities.Customer;
+
+public interface CustomerRepo extends JpaRepository<Customer, Integer>{
+
+	boolean existsCustomerById(int cId);
+
+}
---
+++ re-service/src/main/java/com/getcarloan/reservice/service/CustomerService.java
@@ -0,0 +1,21 @@
+package com.getcarloan.reservice.service;
+
+
+
+import java.util.List;
+
+import com.getcarloan.reservice.entities.Customer;
+
+public interface CustomerService {
+
+	Customer getCustomerById(int cId);
+
+	String saveCustomer(Customer customer);
+
+	List<Customer> getAllCustomer();
+
+	String updateCustomer(Customer customer);
+
+	String deleteCustomer(int cId);
+
+}

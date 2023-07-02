31c44410f3ddb661762d3bb5989e4f0337732c80
Atul@1999 <atulsingle358@gmail.com>
2023-07-02 13:41:39 +0530

    created end point of AccountDetails

---
+++ re-service/src/main/java/com/getcarloan/reservice/repository/AccountDetailsRepo.java
@@ -0,0 +1,17 @@
+package com.getcarloan.reservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.getcarloan.reservice.entities.AccountDetails;
+
+public interface AccountDetailsRepo  extends JpaRepository<AccountDetails, Integer>
+{
+
+	boolean existsgetAllAccountDetailsById(int accountId);
+
+	
+
+	
+	
+
+}
